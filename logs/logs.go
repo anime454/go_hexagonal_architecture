fa0e15db38dc8efa512ea0eceefbacf9ead055e6
thirs204 <thirs204@exten.postbox.in.th>
2022-03-13 04:04:46 +0700

    finished add access in middleware

--- logs/logs.go
+++ logs/logs.go
@@ -20,20 +20,69 @@ func init() {
 	}
 }
 
-func Info(message string, fileds ...zapcore.Field) {
-	log.Info(message, fileds...)
-	// log.With(zap.String("func", funcName)).Info(message, fileds...)
+type AccessLog struct {
+	Ip            string
+	RequestId     string
+	Method        string
+	Url           string
+	RequestBody   string
+	ResponseCode  int
+	ResponseBody  string
+	ResponserTime string
 }
 
-func Debug(message string, fileds ...zapcore.Field) {
-	log.Debug(message, fileds...)
+func RequestLog(ac AccessLog, filed ...zapcore.Field) {
+	c := ac.ResponseCode
+	if c >= 20000 && c <= 39999 {
+		log.With(
+			zap.String("Ip", ac.Ip),
+			zap.String("RequestId", ac.RequestId),
+			zap.String("Method", ac.Method),
+			zap.String("Url", ac.Url),
+			zap.String("RequestBody", ac.RequestBody),
+			zap.Int("ResponseCode", ac.ResponseCode),
+			zap.String("ResponseBody", ac.ResponseBody),
+			zap.String("ResponserTime", ac.ResponserTime),
+		).Info("success", filed...)
+	} else if c >= 40000 && c <= 49999 {
+		log.With(
+			zap.String("Ip", ac.Ip),
+			zap.String("RequestId", ac.RequestId),
+			zap.String("Method", ac.Method),
+			zap.String("Url", ac.Url),
+			zap.String("RequestBody", ac.RequestBody),
+			zap.Int("ResponseCode", ac.ResponseCode),
+			zap.String("ResponseBody", ac.ResponseBody),
+			zap.String("ResponserTime", ac.ResponserTime),
+		).Info("warning", filed...)
+	} else {
+		log.With(
+			zap.String("Ip", ac.Ip),
+			zap.String("RequestId", ac.RequestId),
+			zap.String("Method", ac.Method),
+			zap.String("Url", ac.Url),
+			zap.String("RequestBody", ac.RequestBody),
+			zap.Int("ResponseCode", ac.ResponseCode),
+			zap.String("ResponseBody", ac.ResponseBody),
+			zap.String("ResponserTime", ac.ResponserTime),
+		).Error("error")
+	}
 }
 
-func Error(message interface{}, fileds ...zapcore.Field) {
+func Info(message string, filed ...zapcore.Field) {
+	log.Info(message, filed...)
+	// log.With(zap.String("func", funcName)).Info(message, filed...)
+}
+
+func Debug(message string, filed ...zapcore.Field) {
+	log.Debug(message, filed...)
+}
+
+func Error(message interface{}, filed ...zapcore.Field) {
 	switch t := message.(type) {
 	case error:
-		log.Error(t.Error(), fileds...)
+		log.Error(t.Error(), filed...)
 	case string:
-		log.Error(t, fileds...)
+		log.Error(t, filed...)
 	}
 }
